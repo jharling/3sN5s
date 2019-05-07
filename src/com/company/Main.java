533d27a05145922cda247e37afccd58ef0856ce9
James Harling <jharling@harlingcs.com>
2019-05-06 21:30:54 -0600

    Challenge complete.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        int num = 1000;
+        int answer = 0;
+        answer = numMaker(answer, num-1);
+        System.out.println(answer);
     }
 
-
+    private static Integer numMaker(int answer, int num) {
+        if (num % 3 == 0) {
+            answer = answer + num;
+        } else if (num % 5 == 0) {
+            answer = answer + num;
+        } else if (num < 3) {
+            return answer;
+        }
+        return numMaker(answer, num - 1);
+    }
 }
